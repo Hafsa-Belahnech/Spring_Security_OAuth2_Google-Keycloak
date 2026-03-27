8e3fbb29912b7fe05c7f14c38ac44e6a9125ee1e
Hafsa <250507787+Hafsa-Belahnech@users.noreply.github.com>
2026-03-27 12:21:40 +0100

    Premier commit propre

---
+++ src/main/java/ma/fstg/ClientOauth2Application.java
@@ -0,0 +1,13 @@
+package ma.fstg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientOauth2Application.class, args);
+    }
+
+}
